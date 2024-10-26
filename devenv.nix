13b057d34c4cb70f124923aee1fc58151a4691d4
Neyts Zupan <zupo@users.noreply.github.com>
2024-10-26 12:51:44 +0200

    enforce formatting of nix files

--- devenv.nix
+++ devenv.nix
@@ -1,8 +1,10 @@
 { pkgs, config, inputs, ... }:
 let
-  pkgs-unstable = import inputs.nixpkgs-unstable { system = pkgs.stdenv.system; config.allowUnfree = true;  };
-in
-{
+  pkgs-unstable = import inputs.nixpkgs-unstable {
+    system = pkgs.stdenv.system;
+    config.allowUnfree = true;
+  };
+in {
 
   languages.elm.enable = true;
 
@@ -24,28 +26,30 @@ in
     check-merge-conflicts.enable = true;
     check-shebang-scripts-are-executable.enable = true;
     check-yaml.enable = true;
-    typos.enable = true;
     deadnix.enable = true;
     detect-private-keys.enable = true;
+    nixfmt.enable = true;
     ripsecrets.enable = true;
+    typos.enable = true;
 
     trim-trailing-whitespace.enable = true;
-    trim-trailing-whitespace.excludes = [".elm-land/"];
+    trim-trailing-whitespace.excludes = [ ".elm-land/" ];
 
     end-of-file-fixer.enable = true;
-    end-of-file-fixer.excludes = [".elm-land/"];
+    end-of-file-fixer.excludes = [ ".elm-land/" ];
 
     shellcheck.enable = true;
-    shellcheck.excludes = [".yml" ".yaml"];
+    shellcheck.excludes = [ ".yml" ".yaml" ];
 
     denofmt.enable = true;
-    denofmt.excludes = ["elm.json" "review/elm.json" "elm-land.json" ".elm-land/"];
+    denofmt.excludes =
+      [ "elm.json" "review/elm.json" "elm-land.json" ".elm-land/" ];
 
     elm-format.enable = true;
-    elm-format.excludes = [".elm-land/" "src/Evergreen"];
+    elm-format.excludes = [ ".elm-land/" "src/Evergreen" ];
 
     elm-review.enable = true;
-    elm-review.excludes = [".elm-land/"];
+    elm-review.excludes = [ ".elm-land/" ];
   };
 
   enterShell = ''
@@ -56,16 +60,13 @@ in
     elm-test-rs --compiler $(which lamdera)
   '';
 
-
-  processes =
-    if !config.devenv.isTesting
-    then
-      {
-        elm-land.exec = "elm-land server";
-        lamdera.exec = "lamdera live";
-        tailwind.exec = "tailwindcss -i ./src/style.css -o ./public/style.css --watch";
-      }
-    else {};
+  processes = if !config.devenv.isTesting then {
+    elm-land.exec = "elm-land server";
+    lamdera.exec = "lamdera live";
+    tailwind.exec =
+      "tailwindcss -i ./src/style.css -o ./public/style.css --watch";
+  } else
+    { };
 
   scripts.lint.exec = "pre-commit run --all-files";
   scripts.tests.exec = "elm-test-rs --compiler $(which lamdera)";
